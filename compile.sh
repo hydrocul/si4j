19d2f7fcf7636de398eb9cb68306abbef5e09278
hydrocul <hydrocul@gmail.com>
2010-10-28 01:45:32 +0900

    add replace script to compile.sh

--- compile.sh
+++ compile.sh
@@ -6,6 +6,7 @@ exit 1
 fi
 
 if [ ! -d ./class ] ; then
+
 mkdir ./class
 echo unzip $SCALA_HOME/lib/scala-compiler.jar
 pushd ./class >/dev/null
@@ -13,4 +14,13 @@ unzip $SCALA_HOME/lib/scala-compiler.jar
 rm -r META-INF
 rm compiler.properties
 popd >/dev/null
+
+pushd ./class/scala/tools/nsc/interpreter >/dev/null
+{
+find . -exec echo 'echo create scala/tools/interpretex/{}' \; echo perl -p -e '/scala\\/tools\\/nsc\\/interpreter/scala\\/tools\\/nsc\\/interpretex/g' \< ./class/scala/tools/nsc/interpreter/{} \> ./class/scala/tools/nsc/interpretex/{} \;
+} > build1.tmp
+popd >/dev/null
+
+#. build1.tmp
+
 fi
